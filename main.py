e34116b9012cbb78e1f8a3f6c2eebff6e47e3d51
Bassem Dghaidi <568794+Link-@users.noreply.github.com>
2024-10-22 21:55:48 +0200

    Add button controls

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import badger2040
 import random
 import time
 
-WIDTH      = 45
-HEIGHT     = 12
-CELL       = '+'
-EMPTY_CELL = ' '
-DEBUG      = False
+WIDTH        = 45
+HEIGHT       = 12
+CELL         = '+'
+EMPTY_CELL   = ' '
+DEBUG        = True
+RUNNING      = True
+REFRESH_RATE = 0.5
 
 def initialize_grid():
     return [[random.choice([EMPTY_CELL, CELL]) for _ in range(WIDTH)] for _ in range(HEIGHT)]
@@ -17,14 +19,21 @@ def print_grid(grid):
     badger.set_pen(15)
     badger.set_font("bitmap8")
     badger.set_thickness(1)
+
     for k, row in enumerate(grid):
         badger.text(''.join(row), 0, k*10)
-        
-        if DEBUG:
-            print(k, row)
-
+    
     badger.update()
     
+def write_text(text):
+    badger.set_pen(0)
+    badger.clear()
+    badger.set_pen(15)
+    badger.set_font("bitmap8")
+    badger.set_thickness(1)
+    badger.text(text, 25, 25)
+    badger.update()
+    time.sleep(1)
 
 def count_neighbors(grid, x, y):
     neighbors = [
@@ -59,13 +68,45 @@ def update_grid(grid):
                     new_grid[x][y] = CELL
     return new_grid
                         
-
-# Execute
+# ----------------------------------------------------------------
+# Main loop
+# ----------------------------------------------------------------
 badger = badger2040.Badger2040()
 badger.set_update_speed(2)
 grid = initialize_grid()
 
 while True:
-    print_grid(grid)
-    grid = update_grid(grid)
-    time.sleep(0.5)
+    if badger.pressed(badger2040.BUTTON_A):
+        # Re-initialize the grid with a new random pattern
+        grid = initialize_grid()
+        write_text('Grid re-initialized')
+        if DEBUG:
+            print('BUTTON_A: grid re-initialized')
+    
+    if badger.pressed(badger2040.BUTTON_B):
+        # Toggle the running state
+        RUNNING = not RUNNING
+        write_text('Running: ' + str(RUNNING))
+        if DEBUG:
+            print('BUTTON_B: toggle running state: ', RUNNING)
+
+    if badger.pressed(badger2040.BUTTON_UP):
+        # Increase the refresh rate (lower is faster)
+        if REFRESH_RATE - 0.1 > 0:
+            REFRESH_RATE -= 0.1
+        write_text('Refresh rate: ' + str(REFRESH_RATE))
+        if DEBUG:
+            print('BUTTON_UP: refresh rate inc: ', REFRESH_RATE)
+    
+    if badger.pressed(badger2040.BUTTON_DOWN):
+        # Decrease the refresh rate (higher is slower)
+        REFRESH_RATE += 0.1
+        write_text('Refresh rate: ' + str(REFRESH_RATE))
+        if DEBUG:
+            print('BUTTON_DOWN: refresh rate dec: ', REFRESH_RATE)
+
+    if RUNNING:
+        print_grid(grid)
+        grid = update_grid(grid)
+        time.sleep(REFRESH_RATE)
+
